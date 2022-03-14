c8425dbba85f38d907f9c7109349e12223b127e1
rudolffonseca <rudolffonseca@gmail.com>
2022-03-14 08:39:02 +0100

    login changed to express endpoint

---
+++ routers/login.js
@@ -0,0 +1,37 @@
+const bcrypt = require("bcrypt");
+const { Router } = require("express");
+const { toJWT } = require("../auth/jwt.js");
+const authMiddleware = require("../auth/middleware");
+const Customer = require("../models/").Customer;
+const { SALT_ROUNDS } = require("../config/constants");
+
+const router = new Router();
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .send({ message: "Please provide both email and password" });
+    }
+
+    const user = await Customer.findOne({ where: { email } });
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(400).send({
+        message: "User with that email not found or password incorrect",
+      });
+    }
+
+    delete user.dataValues["password"]; // don't send back the password hash
+    const token = toJWT({ userId: user.id });
+    return res.status(200).send({ token, ...user.dataValues });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({ message: "Something went wrong, sorry" });
+  }
+});
+
+module.exports = router;
